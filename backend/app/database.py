72b5df9f9a60653998fa5d5c9e98b94e644289f3
SABO <Kyuma.K1390@gmail.com>
2025-08-30 13:28:09 +0900

    daatabase url

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from sqlalchemy.engine import URL
 
-SQLALCHEMY_DATABASE_URL = "postgresql://myuser:mypass@db:5432/mydb"
+url = URL.create(
+    drivername="postgresql",   
+    username=os.getenv("POSTGRES_USER", "myuser"),
+    password=os.getenv("POSTGRES_PASSWORD", "mypass"),    
+    host=os.getenv("DB_HOST", "db"),
+    port=int(os.getenv("DB_PORT", "5432")),
+    database=os.getenv("POSTGRES_NAME", "mydb")
+)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(url, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
